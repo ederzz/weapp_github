97a56af1350267a9d80a25c15437edf262454839
shenyiling <jzx19951028yyjy@163.com>
2018-08-31 12:43:42 +0800

    feat: search page

--- src/utils/services.js
+++ src/utils/services.js
@@ -12,3 +12,29 @@ export const login = (username, password) => {
   wx.setStorageSync('base64Account', Base64.encode(`${username}:${password}`))
   return request('/user')
 }
+
+/**
+ * 获取用户starred仓库
+ * @param {Number} pageNo 分页页码
+ * @param {Number} pageSize 分页单页数量
+ */
+export const getUserStarred = (pageNo, pageSize) => {
+  const {
+    login
+  } = wx.getStorageSync('userInfo')
+  return request(`/users/${login}/starredpage=${pageNo}&per_page=${pageSize}`)
+}
+
+/**
+ * 获取某个用户的followers
+ * @param {String} name 用户名
+ */
+export const getUserFollers = name => request(`/user/${name}}/followers`)
+
+/**
+ * 搜索仓库
+ * @param {String} q 搜索关键字
+ * @param {String} sort 排序关键字
+ * @param {String} order 顺序or倒叙
+ */
+export const searchRegistry = (q, sort = '', order = '') => request(`search/repositories?q=${q}&sort=${sort}&order=${order}`)

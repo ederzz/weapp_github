9f0083e97b0708c5a22fdf9dac9114cad477ec0f
shenyiling <jzx19951028yyjy@163.com>
2018-09-16 12:32:29 +0800

    fix: custom_image component

--- src/components/Image/custom_image.js
+++ src/components/Image/custom_image.js
@@ -1,18 +1,18 @@
 Component({
   properties: {
-    imgInfo: {
-      type: Object,
-      value: {
-        src: '',
-        show: false
-      }
+    imgSrc: {
+      type: String,
+      value: ''
     }
+  },
+  data: {
+    show: false
   },
   ready() {
     const intersectionObj = this.createIntersectionObserver()
     intersectionObj.relativeToViewport({ bottom: 100 }).observe('.custom__wrapper', res => {
       this.setData({
-        'imgInfo.show': true
+        'show': true
       }, () => {
         console.log('加载图片')
       })
